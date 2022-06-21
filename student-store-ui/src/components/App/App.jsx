6ac2aced6dc713d083c1429e8a26b3f76e7c9e43
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-06-21 13:59:08 -0700

    Implemented 3 event handlers in App.jsx

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -11,24 +11,99 @@ import NotFound from "../NotFound/NotFound"
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom"
 
+
 export default function App() {
   const [products, setProducts] = React.useState([]);
   const [isFetching, setFetching] = React.useState(true);
   const [error, setError] = React.useState("");
-  const [open, isOpen] = React.useState(false);
+  const [isOpen, setOpen] = React.useState(false);
   const [shoppingCart, setShoppingCart] = React.useState([]);
   const [checkoutForm, setCheckoutForm] = React.useState("");
+  const [shoppingPrice, setShoppingPrice] = React.useState(0);
 
-  
+  React.useEffect(async () => {
+    try {
+      const response = await axios.get("https://codepath-store-api.herokuapp.com/store");
+      const result = response.data.products;
+      setProducts(result);
+      if(products.length == 0) {
+        setError("Products is empty");
+      }
+    }
+    catch (e) {
+      console.log(e);
+      setError(e);
+    }
+  }, []);
+
+  const handleOnToggle = () => {
+    setOpen(false);
+  }
+
+  const handleAddItemToCart = (productId) => {
+    for(let i = 0; i < shoppingCart.length; i++) {
+      if(shoppingCart[i].itemId == productId) {
+        let newCart = [...shoppingCart];
+        newCart[i].quantity+=1;
+        setShoppingCart(newCart);
+        if(products[productId-1].id == productId) {
+          setShoppingPrice(shoppingPrice + products[productId-1].price);
+        }
+        else {
+          console.log("productId assumption failed");
+        }
+        return;
+      }
+    }
+    var newItem = [];
+    newItem[itemId] = productId;
+    newItem[quantity] = 1;
+    setShoppingCart([...shoppingCart, newItem]);
+    if(products[productId-1].id == productId) {
+      setShoppingPrice(shoppingPrice + products[productId-1].price);
+    }
+    else {
+      console.log("productId assumption failed");
+    }
+  }
+
+  const handleRemoveItemFromCart = (productId) => {
+    for(let i = 0; i < shoppingCart.length; i++) {
+      if(shoppingCart[i].itemId == productId) {
+        if(shoppingCart[i].quantity>1) {
+          let newCart = [...shoppingCart];
+          newCart[i].quantity-=1;
+          setShoppingCart(newCart);
+          if(products[productId-1].id == productId) {
+            setShoppingPrice(shoppingPrice - products[productId-1].price);
+          }
+          else {
+            console.log("productId assumption failed");
+          }
+        }
+        else {
+          let newCart = [...shoppingCart];
+          newCart.splice(i, 1);
+          setShoppingCart(newCart);
+          if(products[productId-1].id == productId) {
+            setShoppingPrice(shoppingPrice - products[productId-1].price);
+          }
+          else {
+            console.log("productId assumption failed");
+          }
+        }
+      }
+    }
+  }
 
   return (
     <div className="app">
       <Navbar />
-      <Sidebar />
+      <Sidebar open={isOpen}/>
       <BrowserRouter>
         <main>
           <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route path="/" element={<Home products={products}/>}></Route>
             <Route path="/product/:productId" element={<ProductDetail />}></Route>
             <Route path="*" element={<NotFound />}></Route>
           </Routes>

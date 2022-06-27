b460a48d9f7845b66928ea6cc93cb7e6d7f0dffc
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-06-27 16:05:38 -0700

    Get /store endpoint setup

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,34 @@
 // YOUR CODE HERE
+const express = require('express')
+const morgan = require('morgan')
+const cors = require("cors")
+const app = express();
+const storeRouter = require("./routes/store.js")
+
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json()) //allows us to send objects
+app.use(morgan('tiny')) //used for logging
+
+app.use("/store", storeRouter);
+
+app.get('/', (req, res) => {
+    res.send({"ping": "pong"})
+})
+
+
+app.use((req, res, next) => {
+    return next(new NotFoundError())
+})
+
+app.use((error, req, res, next) => {
+const status = error.status || 500
+const message = error.message
+
+return res.status(status).json({
+    error: { message, status },
+})
+})
+
+module.exports = app;
